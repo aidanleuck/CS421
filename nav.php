45f4c29cf640df85dec390756a69f89f385aa81f
aidanleuck <aidan@leucks.com>
2021-02-08 20:34:07 -0500

    Work on navbar

---
+++ nav.php
@@ -0,0 +1,25 @@
+<html>
+
+    <head>
+    <link rel="stylesheet" href="main.css">
+    </head>
+
+    <body>
+        <nav>
+            <div id = "nav">
+                <ul class = "nav-list">
+                    <li class = "nav-item">Home</li>
+                    <li class = "nav-item">Shop by Product #</li>
+                    <li class = "nav-item">Order Policy</li>
+                    <li class = "nav-item">About Us</li>
+                    <li class = "user-info"><img src = ""/>Cart</li>
+                    <li class = "user-info"><img src = ""/>Car</li>
+                    <li class = "user-info"><img src = ""/>User</li>
+                    
+            </div>
+
+            </ul>
+        </nav>
+    </body>
+
+</html>
